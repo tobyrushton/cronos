d769c45965f3ae25374262f8a2a3979ca6f0ad5e
tobyrushton <tobyjrushton@icloud.com>
2024-07-23 22:40:17 +0100

    check for session during auth load

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,11 @@
+import { validateRequest } from '@/server/auth/validateRequest'
+import { redirect } from 'next/navigation'
 import { FC, ReactNode } from 'react'
 
-const AuthLayout: FC<{ children: ReactNode }> = ({ children }) => {
+const AuthLayout: FC<{ children: ReactNode }> = async ({ children }) => {
+    const session = await validateRequest()
+    if (session.session) redirect('/calendar')
+
     return (
         <div className="flex w-full min-h-screen grid grid-cols-1 md:grid-cols-2">
             <div className="flex h-full">
